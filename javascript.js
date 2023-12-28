58150cd35803bd6990f55e3d02b63909d6e42e1e
Latispq <Latispq@gmail.com>
2023-12-27 23:00:49 -0500

    Added success html and js file

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
+window.onload = function () {
+
+    const form = document.querySelector('#form')
+    const submitButton = document.querySelector('#submit')
+    form.addEventListener('submit', (e) => {
+        submitButton.disabled = true
+        e.preventDefault()
+        window.location.href = window.location.href = 'success.html'
+    })
+
+}
 console.log("Hello World");
